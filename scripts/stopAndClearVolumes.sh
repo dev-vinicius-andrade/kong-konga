acb48129d8240e4f6f1209575e703ccd78929be0
Vinicius Andrade <58332861+dev-vinicius-andrade@users.noreply.github.com>
2023-07-24 14:59:12 -0300

    tests

--- scripts/stopAndClearVolumes.sh
+++ scripts/stopAndClearVolumes.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 docker compose -f docker-compose.yaml --env-file ./.env.local  down -v
-grep -oP '(?<=device: ).*' docker-compose.yaml | while read -r path; do
-  rm -Rf "$path"
-done
+rm -Rf "$1"
--- scripts/stopAsSetupAndClearVolumes.sh
+++ scripts/stopAsSetupAndClearVolumes.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 docker compose --profile setup -f docker-compose.yaml -f docker-compose.setup.yaml --env-file ./.env.local  down -v
-grep -oP '(?<=device: ).*' docker-compose.yaml | while read -r path; do
-  rm -Rf "$path"
-done
+rm -Rf "$1"
