25816b6e62cbaef52a752d894cb8551b2b540fd2
Vinicius Andrade <58332861+dev-vinicius-andrade@users.noreply.github.com>
2023-07-24 15:16:44 -0300

    tests

--- scripts/stopAsSetupAndClearVolumes.sh
+++ scripts/stopAsSetupAndClearVolumes.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 source ./getNamedParameter.sh
-volumes_folder = get_named_parameter "root-folder" "" "${@}"
+volumes_folder=$(get_named_parameter "root-folder" "" "${@}")
 docker compose --profile setup -f docker-compose.yaml -f docker-compose.setup.yaml --env-file ./.env.local  down -v
 rm -Rf "$volumes_folder"
