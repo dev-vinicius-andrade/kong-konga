546be800e3ee42736abd08a6a9a2a84afb57013a
Vinicius Andrade <58332861+dev-vinicius-andrade@users.noreply.github.com>
2023-07-26 02:43:01 -0300

    tests

--- scripts/createEnvironmentLocalFiles.sh
+++ scripts/createEnvironmentLocalFiles.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-source "$(pwd)/helpers.sh"
+source "$(pwd)/scripts/helpers.sh"
 echo "Creating local files" >&2
-copy_file_with_overwrite_question "$(pwd)/../.env.example" "$(pwd)/../.env.local"
-copy_file_with_overwrite_question "$(pwd)/../docker-compose.volumes.yaml" "$(pwd)/../docker-compose.volumes.local.yaml"
-copy_file_with_overwrite_question "$(pwd)/../docker-compose.setup.yaml" "$(pwd)/../docker-compose.setup.local.yaml"
+copy_file_with_overwrite_question "$(pwd)/.env.example" "$(pwd)/.env.local"
+copy_file_with_overwrite_question "$(pwd)/docker-compose.volumes.yaml" "$(pwd)/docker-compose.volumes.local.yaml"
+copy_file_with_overwrite_question "$(pwd)/docker-compose.setup.yaml" "$(pwd)/docker-compose.setup.local.yaml"
